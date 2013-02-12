3863bf262cb8db8546ec7e6fc9402c46fb3b2099
Jared Roesch <roeschinc@gmail.com>
2013-02-11 17:15:39 -0800

    Fixed spaces issue

--- HTTP.scala
+++ HTTP.scala
@@ -16,68 +16,68 @@ object HTTP {
 
 // allows factory like behavior with HTTPConnection
 object HTTPConnection {
-	def apply(url: String) = new HTTPConnection(url)
+  def apply(url: String) = new HTTPConnection(url)
 }
 
 //implements the HTTPConnection class with a connection field and appropriate instance methods 
 class HTTPConnection(url: String) {
-	
+  
   import collection.JavaConversions._
   import java.net._
   import java.io._
   import io.Source.fromInputStream
-	//constructor action 
+  //constructor action 
   private var connection = (new URL(url)).openConnection()
 
   var cookies = Map[String, String]()
   
   def storeCookies = {
-	  connection.getHeaderFields.lift("Set-Cookie") match {
-		  case Some(cookieList) => cookieList foreach { 
-			  c => val (name,value) = c span { _ != '='} 
-			  cookies += name -> (value drop 1)
-			}
+    connection.getHeaderFields.lift("Set-Cookie") match {
+      case Some(cookieList) => cookieList foreach { 
+        c => val (name,value) = c span { _ != '='} 
+        cookies += name -> (value drop 1)
+      }
       case None => 
-		}
+    }
   }
   
   def loadCookies() =
-  	for ((name, value) <- cookies) connection.setRequestProperty("Cookie", name + "=" + value)
+    for ((name, value) <- cookies) connection.setRequestProperty("Cookie", name + "=" + value)
   //URLEncoder.encode is deprecated fix needed
   def post(parameters: Map[String, String]) = {
-		//try {
-			//connection.setRequestProperty("User-Agent")
-			//connection.setConnectTimeout()
+    //try {
+      //connection.setRequestProperty("User-Agent")
+      //connection.setConnectTimeout()
 
-			loadCookies
+      loadCookies
 
-			connection.setDoOutput(true)
-			connection.connect
+      connection.setDoOutput(true)
+      connection.connect
 
-			val postStream = new OutputStreamWriter(connection.getOutputStream())
-			postStream.write(encodePostParameters(parameters).mkString("&"))
-			postStream.flush
-			postStream.close
+      val postStream = new OutputStreamWriter(connection.getOutputStream())
+      postStream.write(encodePostParameters(parameters).mkString("&"))
+      postStream.flush
+      postStream.close
 
-			storeCookies
-			fromInputStream(connection.getInputStream)
-		//} catch {
-			//case e: UnknownHostException => println(e.message + ":Could not connect to host.")
-		//}
+      storeCookies
+      fromInputStream(connection.getInputStream)
+    //} catch {
+      //case e: UnknownHostException => println(e.message + ":Could not connect to host.")
+    //}
   }
 
   private def encodePostParameters(data: Map[String, String]) = 
- 		for ((name,value) <- data) 
-			yield URLEncoder.encode(name) + "=" + URLEncoder.encode(value)
+     for ((name,value) <- data) 
+      yield URLEncoder.encode(name) + "=" + URLEncoder.encode(value)
 
   def get = {
-		//connection.setRequestProperty()
-		//connection.setConnectTimeout()
-		loadCookies
-		connection.connect
-		storeCookies
-		fromInputStream(connection.getInputStream)
-	} 
+    //connection.setRequestProperty()
+    //connection.setConnectTimeout()
+    loadCookies
+    connection.connect
+    storeCookies
+    fromInputStream(connection.getInputStream)
+  } 
 
 }
  
